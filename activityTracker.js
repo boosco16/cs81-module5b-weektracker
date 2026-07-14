345ed264beb2cc311733c9d6272bd13b210e0a26
bosco <frai9628@gmail.com>
2026-07-13 17:29:59 -0700

    Create activityTracker.js with weekly activities data
    Initialized activity tracker for weekly activities with details.

---
+++ activityTracker.js
@@ -0,0 +1,13 @@
+const myWeek = [
+  { day: "Monday", activity: "Wisdom Teeth Removal", category: "physical", hoursSpent: 1.5, enjoyment: 1, timeOfDay: "afternoon" },
+  { day: "Tuesday", activity: "SMC HW", category: "creative", hoursSpent: 3, enjoyment: 5, timeOfDay: "afternoon" },
+  { day: "Wednesday", activity: "Gaming with friends", category: "social", hoursSpent: 3, enjoyment: 10, timeOfDay: "evening" },
+  { day: "Thursday", activity: "Cooking breakfast", category: "creative", hoursSpent: 1, enjoyment: 8, timeOfDay: "morning" },
+  { day: "Friday", activity: "Afternoon walk", category: "physical", hoursSpent: 0.5, enjoyment: 7, timeOfDay: "afternoon" },
+  
+// PREDICTIONS (writing these before I actually run the code) 
+// Highest enjoyment will be Gaming with friends with it being a 10/10. 
+// The creative and physical categories will fight to dominate my week. I'm going to guess creative will be on top. 
+// My peak enjoyment will be focused around evening time, because thats when I've completed all my work and have time to relax. 
+
+  
